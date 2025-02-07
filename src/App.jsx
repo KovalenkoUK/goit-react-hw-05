9555fda8b139aa72716f2b823664672ed35abcf9
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-02-07 23:17:31 +0200

    js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navigation from './components/Navigation';
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 
-const HomePage = lazy(() => import('./pages/HomePage'));
-const MoviesPage = lazy(() => import('./pages/MoviesPage'));
-const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage'));
-const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+const HomePage = lazy(() => import('./HomePage.jsx'));
+const MoviesPage = lazy(() => import('./MoviesPage.jsx'));
+const MovieDetailsPage = lazy(() => import('./MovieDetailsPage.jsx'));
+const NotFoundPage = lazy(() => import('./NotFoundPage.jsx'));
+const MovieReviews = lazy(() => import('./MovieReviews.jsx'));
+const MovieCast = lazy(() => import('./MovieCast.jsx'));
 
-function App() {
+const Navigation = () => (
+  <nav>
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="/movies">Movies</NavLink>
+  </nav>
+);
+
+const App = () => {
   return (
     <Router>
       <Navigation />
@@ -16,11 +24,13 @@ function App() {
           <Route exact path="/" component={HomePage} />
           <Route exact path="/movies" component={MoviesPage} />
           <Route path="/movies/:movieId" component={MovieDetailsPage} />
+          <Route path="/movies/:movieId/reviews" component={MovieReviews} />
+          <Route path="/movies/:movieId/cast" component={MovieCast} />
           <Route component={NotFoundPage} />
         </Switch>
       </Suspense>
     </Router>
   );
-}
+};
 
 export default App;
